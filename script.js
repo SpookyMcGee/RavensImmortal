e6e3b4a57ba87b2e2a4e242a4de1fd5364d39d78
SpookyMcGee <netherlainsean@gmail.com>
2025-06-14 11:18:33 -0700

    new index script and style

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic interactivity (e.g., alert on button click)
+document.querySelectorAll('.profile-actions button').forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Feature under construction!');
+  });
+});
